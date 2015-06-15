675e10e68c21babf2e051bdbd56299366f266242
Luqman Sahidin <lsahidin@yahoo.com>
2015-06-15 17:14:34 +0700

    Create HG7881.ino

---
+++ GcodeCNCDemo2Axis/HG7881.ino
@@ -0,0 +1,63 @@
+//------------------------------------------------------------------------------
+// 2 Axis CNC Demo
+// dan@marginallycelver.com 2013-08-30
+//------------------------------------------------------------------------------
+// Copyright at end of file.
+// please see http://www.github.com/MarginallyClever/GcodeCNCDemo for more information.
+
+#if CONTROLLER == HG7881
+
+//------------------------------------------------------------------------------
+// INCLUDES
+//------------------------------------------------------------------------------
+#include <HG7881Step.h>
+
+
+//------------------------------------------------------------------------------
+// GLOBALS
+//------------------------------------------------------------------------------
+// Initialize Adafruit stepper controller
+// HG7881Step motor(Step, pinA_IA, pinA_IB, pinB_IA, pin_B_IB);
+HG7881Step m1((int)STEPS_PER_TURN, 9, 5, 10, 6);
+HG7881Step m2((int)STEPS_PER_TURN, 0, 6, 5, 7);
+
+
+//------------------------------------------------------------------------------
+// METHODS
+//------------------------------------------------------------------------------
+
+void m1step(int dir) {
+  m1.onestep(dir);
+}
+
+void m2step(int dir) {
+  m2.onestep(dir);
+}
+
+void disable() {
+    m1.release();
+    m2.release();
+}
+
+
+void setup_controller() {}
+
+
+#endif  // CONTROLLER == HG7881
+/**
+* This file is part of GcodeCNCDemo.
+*
+* GcodeCNCDemo is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* GcodeCNCDemo is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+*/
+
